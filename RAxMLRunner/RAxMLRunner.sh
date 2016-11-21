60662bcc1439c6367c44ae6af1b57ec36ad2b9a9
Justin C. Bagley <justincbagley@users.noreply.github.com>
2016-11-21 11:36:02 -0600

    Update RAxMLRunner.sh

--- RAxMLRunner/RAxMLRunner.sh
+++ RAxMLRunner/RAxMLRunner.sh
@@ -4,20 +4,19 @@
 #  __  o  __   __   __  |__   __                                                         #
 # |__) | |  ' (__( |  ) |  ) (__(                                                        # 
 # |                                                                                      #
-#                         RAxMLRunner v1.1, September 2016                               #
+#                          RAxMLRunner v1.2, November 2016                               #
 #   SHELL SCRIPT FOR AUTOMATING MOVING AND RUNNING RAxML RUNS ON A REMOTE SUPERCOMPUTER  #
 #   (AND EXTRACTING THE RESULTS...coming soon)                                           #
 #   Copyright (c)2016 Justin C. Bagley, Universidade de Brasília, Brasília, DF, Brazil.  #
 #   See the README and license files on GitHub (http://github.com/justincbagley) for     #
-#   further information. Last update: September 7, 2016. For questions, please email     #
+#   further information. Last update: November 21, 2016. For questions, please email     #
 #   jcbagley@unb.br.                                                                     #
 ##########################################################################################
 
 echo "
 ##########################################################################################
-#                         RAxMLRunner v1.1, September 2016                               #
-##########################################################################################
-"
+#                          RAxMLRunner v1.2, November 2016                               #
+##########################################################################################"
 
 echo "INFO      | $(date) | STEP #1: SETUP VARIABLES AND CHECK CONTENTS. "
 ##### Setup and run check on the number of run folders in present working directory:
@@ -28,11 +27,11 @@ echo "INFO      | $(date) | STEP #1: SETUP VARIABLES AND CHECK CONTENTS. "
 	}
 	MY_NUM_RUN_FOLDERS="$(calc $MY_DIRCOUNT - 1)"				## We need to subtract one from this count because "find" counts the current directory as well, but we want only the daughter directories within this working directory.
 										## **IMPORTANT**: NOTE we also assume here that every folder in the current directory is a RAxML run folder!!!
-echo "INFO      | $(date) |         Found $MY_NUM_RUN_FOLDERS run folders present in the current working directory. "
+echo "INFO      | $(date) |          Found $MY_NUM_RUN_FOLDERS run folders present in the current working directory. "
 
 
-echo "INFO      | $(date) | STEP #2: MAKE BASH SUBMSSION FILE; MOVE ALL RUN FOLDERS TO SUPERCOMPUTER; AND \
-THEN CHECK THAT BASH SUBMISSION FILE WAS CREATED. "
+echo "INFO      | $(date) | STEP #2: MAKE BATCH SUBMSSION FILE; MOVE ALL RUN FOLDERS TO SUPERCOMPUTER; AND \
+THEN CHECK THAT BATCH SUBMISSION FILE WAS CREATED. "
 ##--This step assumes that you have set up passowordless access to your supercomputer
 ##--account (e.g. passwordless ssh access), by creating and organizing appropriate and
 ##--secure public and private ssh keys on your machine and the remote supercomputer (by 
@@ -52,10 +51,10 @@ THEN CHECK THAT BASH SUBMISSION FILE WAS CREATED. "
 	awk -F"=" '{print $NF}' | sed 's/\ //g')"
 
 	##--Start making batch queue submission file by making just the top with correct shebang:
-echo "#!/bin/sh
-" > batch_qsub_top.txt
+echo "#!/bin/bash
+" > sbatch_sub_top.txt
 
-echo "INFO      | $(date) |         Starting copying run folders to supercomputer... note: this may display folder contents transferred rather than folder names. "
+echo "INFO      | $(date) |          Starting copying run folders to supercomputer... note: this may display folder contents transferred rather than folder names. "
 (
 	for i in ./*/; do
 		FOLDERNAME="$(echo $i | sed 's/\.\///g')"
@@ -68,58 +67,58 @@ sbatch RAxML_sbatch.sh
 	done
 )
 
-##--Finish making batch queue submission file and name it "batch_qsub.sh".
+##--Finish making batch queue submission file and name it "sbatch_sub.sh".
 echo "
 $MY_SC_PBS_WKDIR_CODE
 exit 0
-" > batch_qsub_bottom.txt
+" > sbatch_sub_bottom.txt
 
-cat batch_qsub_top.txt cd_and_sbatch_commands.txt batch_qsub_bottom.txt > raxmlrunner_batch_qsub.sh
+cat sbatch_sub_top.txt cd_and_sbatch_commands.txt sbatch_sub_bottom.txt > raxmlrunner_sbatch_sub.sh
 
-
-##--More flow control. Check to make sure batch_qsub.sh file was successfully created.
-if [ -f ./raxmlrunner_batch_qsub.sh ]; then
-    echo "INFO      | $(date) |         Batch queue submission file ("raxmlrunner_batch_qsub.sh") successfully created. "
+##--More flow control. Check to make sure sbatch_sub.sh file was successfully created.
+if [ -f ./raxmlrunner_sbatch_sub.sh ]; then
+    echo "INFO      | $(date) |          Batch queue submission file ("raxmlrunner_sbatch_sub.sh") successfully created. "
 else
-    echo "INFO      | $(date) |         Something went wrong. Batch queue submission file ("raxmlrunner_batch_qsub.sh") not created. "
+    echo "INFO      | $(date) |          Something went wrong. Batch queue submission file ("raxmlrunner_sbatch_sub.sh") not created. "
 fi
 
-echo "INFO      | $(date) | STEP #3: MOVE BASH SUBMISSION FILE TO SUPERCOMPUTER AND EXECTUE IT! "
-echo "INFO      | $(date) |         Moving batch file to supercomputer and executing it... "
+echo "INFO      | $(date) | STEP #3: MOVE BATCH SUBMISSION FILE TO SUPERCOMPUTER. "
+echo "INFO      | $(date) |          Moving batch file to supercomputer... "
 
 ##### Pull out the correct path to user's bin folder on the supercomputer from the "raxml_runner.cfg" configuration file.
 	MY_SC_BIN="$(grep -n "bin_path" ./raxml_runner.cfg | \
 	awk -F"=" '{print $NF}' | sed 's/\ //g')"
 
-##--This is the key: moving the bash submission file to supercomputer and executing it.
-##--Hopefully, after this, all jobs will be submitted to run on the supercomputer. User
-##--should verify this by checking run folder for correct log files on supercomputer.
-scp ./raxmlrunner_batch_qsub.sh $MY_SSH_ACCOUNT:$MY_SC_BIN
-ssh $MY_SSH_ACCOUNT bash -c "'
-cd $MY_SC_BIN
-chmod +x raxmlrunner_batch_qsub.sh
-source raxmlrunner_batch_qsub.sh
-exit;
-'"
+echo "INFO      | $(date) |          Also copying sbatch_sub_file to supercomputer..."
+scp ./raxmlrunner_sbatch_sub.sh $MY_SSH_ACCOUNT:$MY_SC_DESTINATION
 
-MY_QSUB_COMMANDS=cat cd_and_sbatch_commands.txt
-ssh -t $MY_SSH_ACCOUNT bash -c "'
-$MY_QSUB_COMMANDS
+
+echo "INFO      | $(date) | STEP #4: SUBMIT ALL JOBS TO THE QUEUE. "
+##--This is the key: using ssh to connect to supercomputer, loop through all run folders, 
+##--and submit all jobs/runs (sh scripts in each folder) to the queue. We do this while  
+##--using the ssh "-c" flag to make variable expansion work, so that we can refer to our
+##--destination path and email which we placed into environmental variables above.
+
+ssh $MY_SSH_ACCOUNT '
+cd ADD_PATH_TO_SC_DESTINATION_HERE
+## __PATH NEEDED__: User must change the above line to indicate the absolute path to the same folder that the $MY_SC_DESTINATION environmental variable points to (pulled from 'destination_path' in the cfg file during STEP #1 above).
+chmod u+x raxmlrunner_sbatch_sub.sh
+./raxmlrunner_sbatch_sub.sh
 exit
-'"
+'
 
-	##--Cleanup: remove temporary files on local machine.
-	rm batch_qsub_top.txt
-	rm batch_qsub_bottom.txt
+echo "INFO      | $(date) |          Finished copying run folders to supercomputer and submitting RAxML jobs to queue!!"
+
+echo "INFO      | $(date) |          Cleaning up: removing temporary files from local machine..."
+	rm sbatch_sub_top.txt
+	rm sbatch_sub_bottom.txt
 	#rm cd_and_sbatch_commands.txt
 
 	##--Optional cleanup: remove batch submission script from bin folder on supercomputer account.
-	##--ssh $MY_SSH_ACCOUNT 'rm ~/bin/raxmlrunner_batch_qsub.sh;' (path/to/bin may be different
+	##--ssh $MY_SSH_ACCOUNT 'rm ~/bin/raxmlrunner_sbatch_sub.sh;' NOTE: path/to/bin may be different
 	##--on another user's account.
 
-
-echo "INFO      | $(date) | Finished copying run folders to supercomputer and submitting RAxML jobs to queue!! "
-echo "INFO      | $(date) | Bye.
+echo "INFO      | $(date) |          Bye.
 "
 #
 #
