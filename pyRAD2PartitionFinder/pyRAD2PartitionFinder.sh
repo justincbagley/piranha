af3f587e96131527b6cec09a29a6d76f119d54d2
Justin C. Bagley <justincbagley@users.noreply.github.com>
2017-12-06 09:46:21 -0500

    Update pyRAD2PartitionFinder.sh

--- pyRAD2PartitionFinder/pyRAD2PartitionFinder.sh
+++ pyRAD2PartitionFinder/pyRAD2PartitionFinder.sh
@@ -6,10 +6,9 @@
 # |                                                                                      #
 #                        pyRAD2PartitionFinder v1.1, August 2017                         #
 #  SHELL SCRIPT FOR RUNNING PartitionFinder ON SNP DNA PARTITIONS OUTPUT FROM pyRAD      #
-#  Copyright (c)2017 Justinc C. Bagley, Virginia Commonwealth University, Richmond, VA,  #
-#  USA; Universidade de Brasília, Brasília, DF, Brazil. See README and license on GitHub #
-#  (http://github.com/justincbagley) for further information. Last update: August 1,     #
-#  2017. For questions, please email jcbagley@vcu.edu.                                   #
+#  Copyright ©2017 Justinc C. Bagley. For further information, see README and license    #
+#  available in the PIrANHA repository (https://github.com/justincbagley/PIrANHA/). Last #
+#  update: August 1, 2017. For questions, please email jcbagley@vcu.edu.                 #
 ##########################################################################################
 
 ############ SCRIPT OPTIONS
