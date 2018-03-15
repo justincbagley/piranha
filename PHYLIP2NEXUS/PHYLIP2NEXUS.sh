398fb63f77e021affa314f9a7ca752dda2b53762
Justin C. Bagley <centrarchidae@gmail.com>
2018-03-15 15:30:13 -0400

    Deleted README from script

--- PHYLIP2NEXUS/PHYLIP2NEXUS.sh
+++ PHYLIP2NEXUS/PHYLIP2NEXUS.sh
@@ -12,23 +12,6 @@
 #  update: March 15, 2018. For questions, please email jcbagley@vcu.edu.                 #
 ##########################################################################################
 
-##--README: This shell script converts a single PHYLIP-formatted DNA sequence alignment
-##--file present in the current working directory into NEXUS format. The starting file
-##--must have the extension ".phy", and the first line of this file must contain the 
-##--number of taxa, followed by one space, followed by the number of characters in the 
-##--alignment. Some actions are echoed to screen. The output is a single NEXUS file with
-##--the name, "BASENAME.nex", where "BASENAME" is the base or root name of the original
-##--PHYLIP file. For example, in a starting file named "Smerianae_ND4.phy," BASENAME would 
-##--be "Smerianae_ND4" and the resulting output file would be named "Smerianae_ND4.nex". 
-#
-##--Added two new options, one for specifying a partitions file (-p), and another related
-##--option for specifying the format of the partitions file (-f). If the partitions file is
-##--given, then the script expects partitions file format to be of either RAxML format 
-##--(specified with 'raxml') or NEXUS format ('NEX' or 'nex'). The RAxML format is the same
-##--format used in RAxML and output by PartitionFinder, and both this format and the more
-##--traditional NEXUS charset format (i.e. 'begin sets; ... charset 1 = 1-xxx;') will be 
-##--familiar to most users.
-
 ############ SCRIPT OPTIONS
 ## OPTION DEFAULTS ##
 MY_PARTITIONS_FILE=NULL
