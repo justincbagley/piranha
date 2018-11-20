0d2cfc50e4db8a617b698221fce97a7d48113c63
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-11-20 13:15:22 -0600

    Update RAxMLRunner.sh

--- RAxMLRunner/RAxMLRunner.sh
+++ RAxMLRunner/RAxMLRunner.sh
@@ -5,11 +5,11 @@
 # |__) | |  ' (__( |  ) |  ) (__(                                                        # 
 # |                                                                                      #
 #                              RAxMLRunner v1.3, April 2017                              #
-#   SHELL SCRIPT FOR AUTOMATING MOVING AND RUNNING RAxML RUNS ON A REMOTE SUPERCOMPUTER  #
-#   (AND EXTRACTING THE RESULTS...coming soon)                                           #
+#  SHELL SCRIPT FOR AUTOMATING MOVING AND RUNNING RAxML RUNS ON A REMOTE SUPERCOMPUTER   #
+#  (AND EXTRACTING THE RESULTS...coming soon)                                            #
 #  Copyright ©2017 Justinc C. Bagley. For further information, see README and license    #
 #  available in the PIrANHA repository (https://github.com/justincbagley/PIrANHA/). Last #
-#  update: August 20, 2017. For questions, please email jcbagley@vcu.edu.                #
+#  update: August 20, 2017. For questions, please email bagleyj@umsl.edu.                #
 ##########################################################################################
 
 echo "
