4f14d52a0c692cfcf0d67ac667cca322929dae9d
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-11-20 13:12:12 -0600

    Update BEASTRunner.sh

--- BEASTRunner/BEASTRunner.sh
+++ BEASTRunner/BEASTRunner.sh
@@ -8,7 +8,7 @@
 #  SHELL SCRIPT FOR AUTOMATING RUNNING BEAST ON A REMOTE SUPERCOMPUTING CLUSTER          #
 #  Copyright ©2017 Justinc C. Bagley. For further information, see README and license    #
 #  available in the PIrANHA repository (https://github.com/justincbagley/PIrANHA/). Last #
-#  update: May 3, 2017. For questions, please email jcbagley@vcu.edu.                    #
+#  update: May 3, 2017. For questions, please email bagleyj@umsl.edu.                    #
 ##########################################################################################
 
 ############ SCRIPT OPTIONS
