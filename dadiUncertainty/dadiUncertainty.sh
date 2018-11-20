56ed1e4d9e412b119289e21d63460c27d6835bd5
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-11-20 13:16:47 -0600

    Update dadiUncertainty.sh

--- dadiUncertainty/dadiUncertainty.sh
+++ dadiUncertainty/dadiUncertainty.sh
@@ -10,7 +10,7 @@
 #  AS WELL AS STANDARD DEVIATION ESTIMATION USING THE FIM METHOD (ORIG. DATA ONLY)       #
 #  Copyright ©2017 Justinc C. Bagley. For further information, see README and license    #
 #  available in the PIrANHA repository (https://github.com/justincbagley/PIrANHA/). Last #
-#  update: October 3, 2017. For questions, please email jcbagley@vcu.edu.                #
+#  update: October 3, 2017. For questions, please email bagleyj@umsl.edu.                #
 ##########################################################################################
 
 
