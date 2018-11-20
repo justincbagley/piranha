e56899ca316f73c7105f01eb693ae3b23365816e
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-11-20 13:18:31 -0600

    Update PhyloMapperNullProc.sh

--- PhyloMapperNullProc.sh
+++ PhyloMapperNullProc.sh
@@ -2,7 +2,7 @@
 
 #  Copyright ©2016 Justinc C. Bagley. For further information, see README and license    #
 #  available in the PIrANHA repository (https://github.com/justincbagley/PIrANHA/). Last #
-#  update: October 2016. For questions, please email jcbagley@vcu.edu.                   #
+#  update: October 2016. For questions, please email bagleyj@umsl.edu.                   #
 
 echo "
 ##########################################################################################
