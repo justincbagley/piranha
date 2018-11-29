5f7fc5b29777b4500a7a9ea543a1d5814aa9c520
Justin C. Bagley <justincbagley@users.noreply.github.com>
2018-11-29 12:04:48 -0600

    Update phylipSubsampler.sh

--- phylipSubsampler.sh
+++ phylipSubsampler.sh
@@ -9,7 +9,7 @@
 #  FILES DOWN TO ONE (RANDOM) SEQUENCE PER SPECIES (FOR SPECIES TREE ANALYSIS)           #
 #  Copyright ©2017 Justinc C. Bagley. For further information, see README and license    #
 #  available in the PIrANHA repository (https://github.com/justincbagley/PIrANHA/). Last #
-#  update: September 27, 2017. For questions, please email bagleyj@umsl.edu.             #
+#  update: November 29, 2018. For questions, please email bagleyj@umsl.edu.              #
 ##########################################################################################
 
 echo "
