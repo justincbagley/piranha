98628f2525c118d7fba4d44078dde6f0cd6d43fe
Justin C. Bagley <justincbagley@users.noreply.github.com>
2019-03-06 22:59:56 -0600

    Update vcfSubsampler.sh

--- vcfSubsampler.sh
+++ vcfSubsampler.sh
@@ -8,7 +8,7 @@
 # File: vcfSubsampler.sh                                                                 #
   VERSION="v0.1.0"                                                                       #
 # Author: Justin C. Bagley                                                               #
-# Date: created by Justin Bagley on Thu Sep 28 09:31:25 2017 -0400                       #
+# Date: created by Justin Bagley on Thu Sep 28 09:31:25 2017 -0400.                      #
 # Last update: March 3, 2019                                                             #
 # Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.                         #
 # Please report bugs to <bagleyj@umsl.edu>.                                              #
@@ -27,9 +27,9 @@ MY_OUTPUT_VCF_FILE=subsample
 DELETE_ORIG_VCF=0
 
 ############ CREATE USAGE & HELP TEXTS
-USAGE="Usage: $(basename "$0") [Help: -h help] [Options: -s o d] [stdin:] <inputVCFFile>
+USAGE="Usage: $(basename $0) [Help: -h help] [Options: -s o d V --version] [stdin:] <inputVCFFile>
  ## Help:
-  -h   help text (also: -help)
+  -h   help text (also: --help) echo this help text and exit
 
  ## Options:
   -s snpsFile (def: $MY_SNPS_FILE) file listing, one per line, names of SNPs that the user 
@@ -37,18 +37,19 @@ USAGE="Usage: $(basename "$0") [Help: -h help] [Options: -s o d] [stdin:] <input
   -o output (def: $MY_OUTPUT_FILE) output vcf file basename (root; not including extension)
   -d deleteOrig (def: 0=no, 1=yes) specifies whether or not to delete the original input
      .vcf file
+  -V   version (also: --version) echo version and exit
 
  CITATION
- Bagley, J.C. 2017. PIrANHA. GitHub package, Available at: 
+ Bagley, J.C. 2019. PIrANHA v0.1.7. GitHub package, Available at: 
 	<http://github.com/justincbagley/PIrANHA>.
  or
- Bagley, J.C. 2017. PIrANHA. [Data set] Zenodo, Available at: 
+ Bagley, J.C. 2019. PIrANHA v0.1.7. [Data set] Zenodo, Available at: 
 	<http://doi.org/10.5281/zenodo.596766>.
  or
- Bagley, J.C. 2017. justincbagley/PIrANHA. GitHub package, Available at: 
+ Bagley, J.C. 2019. justincbagley/PIrANHA. GitHub package, Available at: 
 	<http://doi.org/10.5281/zenodo.596766>.
 
-Created by Justin Bagley on Thu Sep 28 09:31:25 2017 -0400
+Created by Justin Bagley on Thu Sep 28 09:31:25 2017 -0400.
 Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.
 "
 
@@ -57,7 +58,7 @@ if [[ "$1" == "-h" ]] || [[ "$1" == "-help" ]]; then
 	exit
 fi
 
-if [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
+if [[ "$1" == "-V" ]] || [[ "$1" == "--version" ]]; then
 	echo "$(basename $0) $VERSION";
 	exit
 fi
@@ -65,12 +66,10 @@ fi
 ############ PARSE THE OPTIONS
 while getopts 's:o:d:' opt ; do
   case $opt in
-
 ## vcfSubsampler options:
     s) MY_SNPS_FILE=$OPTARG ;;
     o) MY_OUTPUT_FILE=$OPTARG ;;
     d) DELETE_ORIG_VCF=$OPTARG ;;
-
 ## Missing and illegal options:
     :) printf "Missing argument for -%s\n" "$OPTARG" >&2
        echo "$USAGE" >&2
@@ -85,8 +84,8 @@ done
 shift $((OPTIND-1)) 
 # Check for mandatory positional parameters
 if [ $# -lt 1 ]; then
-echo "$USAGE"
-  exit 1
+	echo "$USAGE"
+	exit 1
 fi
 ## Make input file a mandatory parameter:
 MY_INPUT_VCF="$1"
@@ -96,14 +95,14 @@ MY_INPUT_VCF="$1"
 
 ##--Subsample input .vcf file using snps list:
 	while read j; do 
-		grep -h "$(echo $j)" ./"$MY_INPUT_VCF" >> "$MY_OUTPUT_VCF_FILE".tmp; 
+		grep -h "$(echo $j)" ./"$MY_INPUT_VCF" >> "$MY_OUTPUT_VCF_FILE".tmp ; 
 	done < ./"$MY_SNPS_FILE"
 
 
 ##--Add header from original .vcf to the new, subsampled vcf file:
-	grep -h "\#" ./"$MY_INPUT_VCF" > ./header.tmp
+	grep -h "\#" ./"$MY_INPUT_VCF" > ./header.tmp ;
 
-	cat ./header.tmp "$MY_OUTPUT_VCF_FILE".tmp > "$MY_OUTPUT_VCF_FILE".vcf; 
+	cat ./header.tmp "$MY_OUTPUT_VCF_FILE".tmp > "$MY_OUTPUT_VCF_FILE".vcf ; 
 
 
 ##--Cleanup temporary files:
@@ -112,9 +111,9 @@ MY_INPUT_VCF="$1"
 ##--Final cleanup:
 ##--Delete the original .vcf file if user has specified to do so:
 	if [[ "$DELETE_ORIG_VCF" = 0 ]]; then
-		echo ""
+		echo "" ;
 	elif [[ "$DELETE_ORIG_VCF" = 1 ]]; then
-		rm ./"$MY_INPUT_VCF"
+		rm ./"$MY_INPUT_VCF" ;
 	fi
 
 #
