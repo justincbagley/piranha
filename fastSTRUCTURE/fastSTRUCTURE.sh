f3d3818ee60cd812a1061958a30c76540bd2b356
Justin C. Bagley <justincbagley@users.noreply.github.com>
2019-03-06 22:42:14 -0600

    Update fastSTRUCTURE.sh

--- fastSTRUCTURE/fastSTRUCTURE.sh
+++ fastSTRUCTURE/fastSTRUCTURE.sh
@@ -8,7 +8,7 @@
 # File: fastSTRUCTURE.sh                                                                 #
   VERSION="v1.1"                                                                         #
 # Author: Justin C. Bagley                                                               #
-# Date: created by Justin Bagley on Wed, 27 Jul 2016 00:48:14 -0300                      #
+# Date: Created by Justin Bagley on Wed, 27 Jul 2016 00:48:14 -0300.                     #
 # Last update: September 7, 2016                                                         #
 # Copyright (c) 2016-2019 Justin C. Bagley. All rights reserved.                         #
 # Please report bugs to <bagleyj@umsl.edu>.                                              #
@@ -19,7 +19,7 @@
 #                                                                                        #
 ##########################################################################################
 
-if [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
+if [[ "$1" == "-V" ]] || [[ "$1" == "--version" ]]; then
 	echo "$(basename $0) $VERSION";
 	exit
 fi
@@ -32,7 +32,7 @@ echo "
 
 ######################################## START ###########################################
 echo "INFO      | $(date) | STEP 1. SETUP: READ USER INPUT, SET VARIABLES. "
-	MY_FASTSTRUCTURE_WKDIR="$(pwd)"
+	MY_FASTSTRUCTURE_WKDIR="$(pwd)" ;
 
 	read -p "INPUT     | $(date) |         Enter the path to a working copy of fast structure on your machine, \
 e.g. '/Applications/STRUCTURE-fastStructure-e47212f/structure.py' : " fsPATH 
@@ -45,7 +45,7 @@ e.g. '/Applications/STRUCTURE-fastStructure-e47212f/structure.py' : " fsPATH
 
 	read -p "INPUT     | $(date) |         Specify a name (e.g. hypostomus_noout_simple) for the output: " fsOutput 
 
-	MY_FASTSTRUCTURE_PATH="$(echo $fsPATH)"
+	MY_FASTSTRUCTURE_PATH="$(echo $fsPATH)" ;
 
 
 echo "INFO      | $(date) | STEP 2. RUN fastSTRUCTURE ON RANGE OF K SPECIFIED BY USER. "
@@ -54,7 +54,7 @@ echo "INFO      | $(date) |         Modeling K = $lK to $uK clusters in fastSTRU
 (
 	for (( i=$lK; i<=$uK; i++ )); do
 		echo "$i";
-		python "$MY_FASTSTRUCTURE_PATH" -K "$i" --input="$MY_FASTSTRUCTURE_WKDIR/$fsInput" --output="$fsOutput" --format=str --full --seed=100
+		python "$MY_FASTSTRUCTURE_PATH" -K "$i" --input="$MY_FASTSTRUCTURE_WKDIR/$fsInput" --output="$fsOutput" --format=str --full --seed=100 ;
 	done
 )
 
@@ -63,24 +63,24 @@ echo "INFO      | $(date) |         fastSTRUCTURE runs completed. "
 
 echo "INFO      | $(date) | STEP 3. MODEL COMPLEXITY. "
 ###### Obtain an estimate of the model complexity for each set of runs (per species):
-	MY_CHOOSEK_PATH="$(echo $fsPATH | sed 's/structure.py//g' | sed 's/$/chooseK.py/g')"
+	MY_CHOOSEK_PATH="$(echo $fsPATH | sed 's/structure.py//g' | sed 's/$/chooseK.py/g')" ;
 
-	python "$MY_CHOOSEK_PATH" --input="$fsOutput" > chooseK.out.txt
+	python "$MY_CHOOSEK_PATH" --input="$fsOutput" > chooseK.out.txt ;
 
 echo "INFO      | $(date) |         Finished estimating model complexity. "
-	cat chooseK.out.txt
+	cat chooseK.out.txt ;
 
 
 echo "INFO      | $(date) | STEP 4. VISUALIZE RESULTS. "
 ###### Use DISTRUCT to create graphical output of results corresponding to the best K value modeled.
-	read -p "INPUT     | $(date) |         Enter the value of K that you want to visualize : " bestK
+	read -p "INPUT     | $(date) |         Enter the value of K that you want to visualize : " bestK ;
 
-	MY_DISTRUCT_PATH="$(echo $fsPATH | sed 's/structure.py//g' | sed 's/$/distruct.py/g')"
+	MY_DISTRUCT_PATH="$(echo $fsPATH | sed 's/structure.py//g' | sed 's/$/distruct.py/g')" ;
 
-	python "$MY_DISTRUCT_PATH" -K "$bestK" --input="$MY_FASTSTRUCTURE_WKDIR/$fsOutput" --output="$fsOutput_distruct.svg"
+	python "$MY_DISTRUCT_PATH" -K "$bestK" --input="$MY_FASTSTRUCTURE_WKDIR/$fsOutput" --output="$fsOutput_distruct.svg" ;
 
 
-echo "INFO      | $(date) |         Done!!! fastSTRUCTURE analysis complete."
+echo "INFO      | $(date) | Done!!! fastSTRUCTURE analysis complete."
 echo "Bye.
 "
 #
