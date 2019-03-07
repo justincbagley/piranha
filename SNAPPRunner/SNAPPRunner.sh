8a51f51550ac7ef3ee0505685930583727153b37
Justin C. Bagley <justincbagley@users.noreply.github.com>
2019-03-06 21:57:02 -0600

    Update SNAPPRunner.sh

--- SNAPPRunner/SNAPPRunner.sh
+++ SNAPPRunner/SNAPPRunner.sh
@@ -6,12 +6,12 @@
 # |                                                                                      #
 #                                                                                        #
 # File: SNAPPRunner.sh                                                                   #
-  version="v1.0"                                                                         #
+  VERSION="v1.0.1"                                                                       #
 # Author: Justin C. Bagley                                                               #
-# Date: created by Justin Bagley on Thu, 11 May 2017 07:48:55 -0400                      #
-# Last update: February 24, 2019                                                         #
+# Date: Created by Justin Bagley on Thu, 11 May 2017 07:48:55 -0400.                     #
+# Last update: March 6, 2019                                                             #
 # Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.                         #
-# Please report bugs to <bagleyj@umsl.edu>                                               #
+# Please report bugs to <bagleyj@umsl.edu>.                                              #
 #                                                                                        #
 # Description:                                                                           #
 # SHELL SCRIPT FOR AUTOMATING RUNNING SNAPP ON A REMOTE SUPERCOMPUTING CLUSTER           #
@@ -27,10 +27,10 @@ MY_SC_PARTITION=NULL
 MY_STARTING_SEED=NULL
 
 ############ CREATE USAGE & HELP TEXTS
-Usage="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s] [stdin:] <workingDir> 
+USAGE="Usage: $(basename $0) [Help: -h help H Help] [Options: -n w m p s V --version] [stdin:] <workingDir> 
  ## Help:
-  -h   help text (also: -help)
-  -H   verbose help text (also: -Help)
+  -h   help text (also: -help) echo this help text and exit
+  -H   verbose help text (also: -Help) echo verbose help text and exit
 
  ## Options:
   -n   nRuns (def: $MY_NUM_INDEP_RUNS) number of independent SNAPP runs per model (.xml file)
@@ -41,6 +41,7 @@ Usage="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s] [std
   -p   partition (def: NULL) name of supercomputer partition (i.e. group of nodes, or job
        queue controlling a specific group of nodes) to use
   -s   seed (def: NULL) value of random number seed to start BEAST with
+  -V   version (also: --version) echo version and exit
   
  OVERVIEW
  THIS SCRIPT automates conducting multiple runs of SNAPP (Bryant et al. 2012; part of BEAST2, 
@@ -53,7 +54,7 @@ Usage="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s] [std
  identified and run through the SNAPPRunner pipeline, which involves five steps, as follows: 
  1) set up the workspace; 2) copy each XML file n times to create n+1 separate run XML files; 
  3) make directories for 1 run per XML (nRuns) and create a shell script with the name 
- "snapp_sbatch.sh" that is specific to the input and can be used to submit job to supercomputer 
+ 'snapp_sbatch.sh' that is specific to the input and can be used to submit job to supercomputer 
  and move this PBS shell script into the corresponding folder; 4) create a batch submission 
  file (PBS format) and move it and all run folders to the desired working directory on the 
  supercomputer; 5) execute the batch submission file on the supercomputer so that all jobs 
@@ -61,14 +62,14 @@ Usage="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s] [std
 
  Like other software programs such as BPP, G-PhoCS, and GARLI, some information that is used
  by SNAPPRunner.sh is fed to the program by culling the data from an external configuration 
- file, named "snapp_runner.cfg". There are six entries that users can supply in this file. 
+ file, named 'snapp_runner.cfg'. There are six entries that users can supply in this file. 
  However, three of these are essential for running SNAPP using the SNAPPRunner script, 
  including: ssh user account info, the path to the parent directory for SNAPP runs on the 
  supercomputer, the user's email address, and the absolute path to the beast.jar file on 
  the supercomputer that you wish to use. Users must fill this information in and save 
  a new version of the .cfg file in the working directory on their local machine prior to 
  calling the program. Only then can the user call the program by opening a terminal window, 
- typing "./SNAPPRunner.sh", and pressing return.
+ typing './SNAPPRunner.sh', and pressing return.
  
  It is assumed that BEAST2 (e.g. 2.4.2++) is installed on the supercomputer, and that the user 
  can provide absolute paths to the BEAST jar file in the cfg file. Last testing was conducted
@@ -88,11 +89,11 @@ Usage="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s] [std
  Drummond AJ, Suchard MA, Xie D, Rambaut A (2012) Bayesian phylogenetics with BEAUti and 
  	the BEAST 1.7. Molecular Biology and Evolution, 29, 1969-1973.
 
-Created by Justin Bagley on Thu, 11 May 2017 07:48:55 -0400
+Created by Justin Bagley on Thu, 11 May 2017 07:48:55 -0400.
 Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.
 "
 
-verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s] [stdin:] <workingDir> 
+VERBOSE_USAGE="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s] [stdin:] <workingDir> 
  ## Help:
   -h   help text (also: -help)
   -H   verbose help text (also: -Help)
@@ -118,7 +119,7 @@ verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s
  identified and run through the SNAPPRunner pipeline, which involves five steps, as follows: 
  1) set up the workspace; 2) copy each XML file n times to create n+1 separate run XML files; 
  3) make directories for 1 run per XML (nRuns) and create a shell script with the name 
- "snapp_sbatch.sh" that is specific to the input and can be used to submit job to supercomputer 
+ 'snapp_sbatch.sh' that is specific to the input and can be used to submit job to supercomputer 
  and move this PBS shell script into the corresponding folder; 4) create a batch submission 
  file (PBS format) and move it and all run folders to the desired working directory on the 
  supercomputer; 5) execute the batch submission file on the supercomputer so that all jobs 
@@ -126,14 +127,14 @@ verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s
 
  Like other software programs such as BPP, G-PhoCS, and GARLI, some information that is used
  by SNAPPRunner.sh is fed to the program by culling the data from an external configuration 
- file, named "snapp_runner.cfg". There are six entries that users can supply in this file. 
+ file, named 'snapp_runner.cfg'. There are six entries that users can supply in this file. 
  However, three of these are essential for running SNAPP using the SNAPPRunner script, 
  including: ssh user account info, the path to the parent directory for SNAPP runs on the 
  supercomputer, the user's email address, and the absolute path to the beast.jar file on 
  the supercomputer that you wish to use. Users must fill this information in and save 
  a new version of the .cfg file in the working directory on their local machine prior to 
  calling the program. Only then can the user call the program by opening a terminal window, 
- typing "./SNAPPRunner.sh", and pressing return.
+ typing './SNAPPRunner.sh', and pressing return.
  
  It is assumed that BEAST2 (e.g. 2.4.2++) is installed on the supercomputer, and that the user 
  can provide absolute paths to the BEAST jar file in the cfg file. Last testing was conducted
@@ -155,11 +156,11 @@ verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s
  or 5120M, or in terms of GB units: 1G, 2G, 3G, 4G, 5G.
  
 		## Usage examples: 
-		"$0" .
-		"$0" -n 5 .
-		"$0" -n 20 .				## Ex.: changing number of runs.
-		"$0" -n 20 -w 24:00:00 .		## Ex.: changing run walltime.
-		"$0" -n 20 -w 24:00:00 -m 2048M .	## Ex.: changing memory (RAM) allocation.
+		$0 .
+		$0 -n 5 .
+		$0 -n 20 .				## Ex.: changing number of runs.
+		$0 -n 20 -w 24:00:00 .		## Ex.: changing run walltime.
+		$0 -n 20 -w 24:00:00 -m 2048M .	## Ex.: changing memory (RAM) allocation.
 	
  CITATION
  Bagley, J.C. 2019. PIrANHA v0.1.7. GitHub repository, Available at: 
@@ -175,42 +176,40 @@ verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -n w m p s
  Drummond AJ, Suchard MA, Xie D, Rambaut A (2012) Bayesian phylogenetics with BEAUti and 
  	the BEAST 1.7. Molecular Biology and Evolution, 29, 1969-1973.
 
-Created by Justin Bagley on Thu, 11 May 2017 07:48:55 -0400
+Created by Justin Bagley on Thu, 11 May 2017 07:48:55 -0400.
 Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.
 "
 
 if [[ "$1" == "-h" ]] || [[ "$1" == "-help" ]]; then
-	echo "$Usage"
+	echo "$USAGE"
 	exit
 fi
 
 if [[ "$1" == "-H" ]] || [[ "$1" == "-Help" ]]; then
-	echo "$verboseHelp"
+	echo "$VERBOSE_USAGE"
 	exit
 fi
 
-if [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
-	echo "$(basename $0) ${version}";
+if [[ "$1" == "-V" ]] || [[ "$1" == "--version" ]]; then
+	echo "$(basename $0) $VERSION";
 	exit
 fi
 
 ############ PARSE THE OPTIONS
 while getopts 'n:w:m:p:' opt ; do
   case $opt in
-
 ## SNAPPRunner options:
     n) MY_NUM_INDEP_RUNS=$OPTARG ;;
     w) MY_SC_WALLTIME=$OPTARG ;;
     m) JAVA_MEM_ALLOC=$OPTARG ;;
     p) MY_SC_PARTITION=$OPTARG ;;
     s) MY_STARTING_SEED=$OPTARG ;;
-
 ## Missing and illegal options:
     :) printf "Missing argument for -%s\n" "$OPTARG" >&2
-       echo "$Usage" >&2
+       echo "$USAGE" >&2
        exit 1 ;;
    \?) printf "Illegal option: -%s\n" "$OPTARG" >&2
-       echo "$Usage" >&2
+       echo "$USAGE" >&2
        exit 1 ;;
   esac
 done
@@ -219,44 +218,44 @@ done
 shift $((OPTIND-1))
 # Check for mandatory positional parameters
 if [ $# -lt 1 ]; then
-	echo "$Usage"
+	echo "$USAGE"
 	exit 1
 fi
 USER_SPEC_PATH="$1"
 
-echo "INFO      | $(date) |          Setting user-specified path to: "
-echo "$USER_SPEC_PATH "	
-
 echo "
 ##########################################################################################
-#                            SNAPPRunner v1.0, February 2019                             #
-##########################################################################################"
+#                             SNAPPRunner v1.0.1, March 2019                             #
+##########################################################################################
+"
 
 ######################################## START ###########################################
-	calc () {
-	   	bc -l <<< "$@"
-}
 echo "INFO      | $(date) | Starting SNAPPRunner pipeline... "
 echo "INFO      | $(date) | STEP #1: SETUP VARIABLES, MAKE $(calc $MY_NUM_INDEP_RUNS - 1) COPIES PER INPUT XML FILE \
 FOR A TOTAL OF FIVE RUNS OF EACH MODEL/XML USING"
 echo "INFO      | $(date) |          DIFFERENT RANDOM SEEDS. "
-echo "INFO      | $(date) |          Setting up variables, including those specified in the cfg file..."
-	MY_XML_FILES=./*.xml
-	MY_NUM_XML="$(ls . | grep "\.xml$" | wc -l)"
+echo "INFO      | $(date) |          Setting user-specified path to: "
+echo "$USER_SPEC_PATH "	
+	calc () {
+	   	bc -l <<< "$@"
+}
+echo "INFO      | $(date) |          Setting up variables, including those specified in the .cfg file..."
+	MY_XML_FILES=./*.xml ;
+	MY_NUM_XML="$(ls . | grep "\.xml$" | wc -l)";
 echo "INFO      | $(date) |          Number of XML files read: $MY_NUM_XML"	## Check number of input files read into program.
 
 	MY_SSH_ACCOUNT="$(grep -n "ssh_account" ./snapp_runner.cfg | \
-	awk -F"=" '{print $NF}')"
+	awk -F"=" '{print $NF}')";
 	MY_SC_BIN="$(grep -n "bin_path" ./snapp_runner.cfg | \
-	awk -F"=" '{print $NF}' | sed 's/\ //g')"
+	awk -F"=" '{print $NF}' | sed 's/\ //g')";
 	MY_EMAIL_ACCOUNT="$(grep -n "email_account" ./snapp_runner.cfg | \
-	awk -F"=" '{print $NF}')"
+	awk -F"=" '{print $NF}')";
 	MY_SC_DESTINATION="$(grep -n "destination_path" ./snapp_runner.cfg | \
-	awk -F"=" '{print $NF}' | sed 's/\ //g')"					## This pulls out the correct destination path on the supercomputer from the "snapp_runner.cfg" configuration file in the working directory (generated/modified by user prior to running SNAPPRunner).
+	awk -F"=" '{print $NF}' | sed 's/\ //g')";					## This pulls out the correct destination path on the supercomputer from the "snapp_runner.cfg" configuration file in the working directory (generated/modified by user prior to running SNAPPRunner).
 	MY_BEAST_PATH="$(grep -n "beast_jar_path" ./snapp_runner.cfg | \
-	awk -F"=" '{print $NF}')"
+	awk -F"=" '{print $NF}')";
 	MY_SC_PBS_WKDIR_CODE="$(grep -n "pbs_wkdir_code" ./snapp_runner.cfg | \
-	awk -F"=" '{print $NF}')"
+	awk -F"=" '{print $NF}')";
 
 
 echo "INFO      | $(date) | STEP #2: MAKE $(calc $MY_NUM_INDEP_RUNS - 1) COPIES PER INPUT .XML FILE FOR A TOTAL OF $MY_NUM_INDEP_RUNS RUNS OF EACH MODEL or "
@@ -281,7 +280,7 @@ end in 'run.xml'."
 	done
 )
 
-	rm ./*run.xml       ## Remove the original "run.xml" input files so that only XML files
+	rm ./*run.xml ;      ## Remove the original "run.xml" input files so that only XML files
 	            		## annotated with their run numbers 1 - $MY_NUM_INDEP_RUNS remain.
 
 
@@ -300,8 +299,8 @@ if [[ "$MY_SC_PARTITION" = "NULL" ]] && [[ "$MY_STARTING_SEED" = "NULL" ]]; then
 
 (
 	for i in $MY_XML_FILES; do
-		mkdir "$(ls ${i} | sed 's/\.xml$//g')"
-		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')"
+		mkdir "$(ls ${i} | sed 's/\.xml$//g')" ;
+		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')" ;
 
 echo "#!/bin/bash
 
@@ -330,9 +329,9 @@ $MY_SC_PBS_WKDIR_CODE
 
 exit 0" > snapp_sbatch.sh
 
-	chmod +x snapp_sbatch.sh
-	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')"
-	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')"
+	chmod +x snapp_sbatch.sh ;
+	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')" ;
+	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')" ;
 
 	done
 )
@@ -341,8 +340,8 @@ elif [[ "$MY_SC_PARTITION" = "NULL" ]] && [[ "$MY_STARTING_SEED" != "NULL" ]]; t
 
 (
 	for i in $MY_XML_FILES; do
-		mkdir "$(ls ${i} | sed 's/\.xml$//g')"
-		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')"
+		mkdir "$(ls ${i} | sed 's/\.xml$//g')";
+		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')";
 
 echo "#!/bin/bash
 
@@ -371,9 +370,9 @@ $MY_SC_PBS_WKDIR_CODE
 
 exit 0" > snapp_sbatch.sh
 
-	chmod +x snapp_sbatch.sh
-	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')"
-	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')"
+	chmod +x snapp_sbatch.sh ;
+	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')" ;
+	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')" ;
 
 	done
 )
@@ -382,8 +381,8 @@ elif [[ "$MY_SC_PARTITION" != "NULL" ]] && [[ "$MY_STARTING_SEED" = "NULL" ]]; t
 
 (
 	for i in $MY_XML_FILES; do
-		mkdir "$(ls ${i} | sed 's/\.xml$//g')"
-		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')"
+		mkdir "$(ls ${i} | sed 's/\.xml$//g')" ;
+		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')" ;
 
 echo "#!/bin/bash
 
@@ -413,9 +412,9 @@ $MY_SC_PBS_WKDIR_CODE
 
 exit 0" > snapp_sbatch.sh
 
-	chmod +x snapp_sbatch.sh
-	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')"
-	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')"
+	chmod +x snapp_sbatch.sh ;
+	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')" ;
+	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')" ;
 
 	done
 )
@@ -424,8 +423,8 @@ elif [[ "$MY_SC_PARTITION" != "NULL" ]] && [[ "$MY_STARTING_SEED" != "NULL" ]]; 
 
 (
 	for i in $MY_XML_FILES; do
-		mkdir "$(ls ${i} | sed 's/\.xml$//g')"
-		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')"
+		mkdir "$(ls ${i} | sed 's/\.xml$//g')" ;
+		MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')" ;
 
 echo "#!/bin/bash
 
@@ -455,9 +454,9 @@ $MY_SC_PBS_WKDIR_CODE
 
 exit 0" > snapp_sbatch.sh
 
-	chmod +x snapp_sbatch.sh
-	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')"
-	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')"
+	chmod +x snapp_sbatch.sh ;
+	mv ./snapp_sbatch.sh ./"$(ls ${i} | sed 's/.xml$//g')" ;
+	cp $i ./"$(ls ${i} | sed 's/\.xml$//g')" ;
 
 	done
 )
@@ -465,10 +464,8 @@ exit 0" > snapp_sbatch.sh
 fi
 
 echo "INFO      | $(date) |          Setup and run check on the number of run folders created by the program..."
-	MY_FILECOUNT="$(find . -type f | wc -l)"
-	MY_DIRCOUNT="$(find . -type d | wc -l)"
-	MY_NUM_RUN_FOLDERS="$(calc $MY_DIRCOUNT - 1)"
-	#"$(ls . | grep "./*/" | wc -l)"
+	MY_DIRCOUNT="$(find . -type d | wc -l)";
+	MY_NUM_RUN_FOLDERS="$(calc $MY_DIRCOUNT - 1)";
 echo "INFO      | $(date) |          Number of run folders created: $MY_NUM_RUN_FOLDERS"
 
 
@@ -491,8 +488,8 @@ echo "#!/bin/bash
 
 (
 	for j in ./*/; do
-		FOLDERNAME="$(echo $j | sed 's/\.\///g')"
-		scp -r $j $MY_SSH_ACCOUNT:$MY_SC_DESTINATION	## Safe copy to remote machine.
+		FOLDERNAME="$(echo $j | sed 's/\.\///g')" ;
+		scp -r $j $MY_SSH_ACCOUNT:$MY_SC_DESTINATION ;	## Safe copy to remote machine.
 
 echo "cd $MY_SC_DESTINATION$FOLDERNAME
 sbatch snapp_sbatch.sh
@@ -500,13 +497,12 @@ sbatch snapp_sbatch.sh
 
 	done
 )
-### NOT RUN. sed -i '' 1,3d ./cd_and_qsub_commands.txt		## First line of file attempts to cd to "./*/", but this will break the submission script. Here, we use sed to remove the first three lines, which contain the problematic cd line, a sbatch line, and a blank line afterwards.
 
 echo "
 $MY_SC_PBS_WKDIR_CODE
 exit 0
 " > qsub_bottom.txt
-cat qsub_top.txt cd_and_qsub_commands.txt qsub_bottom.txt > SNAPPRunner_batch_qsub.sh
+cat qsub_top.txt cd_and_qsub_commands.txt qsub_bottom.txt > SNAPPRunner_batch_qsub.sh ;
 
 ##--More flow control. Check to make sure sbatch.sh file was successfully created.
 if [ -f ./SNAPPRunner_batch_qsub.sh ]; then
@@ -517,10 +513,10 @@ else
 fi
 
 echo "INFO      | $(date) |          Also copying configuration file to supercomputer..."
-scp ./snapp_runner.cfg $MY_SSH_ACCOUNT:$MY_SC_DESTINATION
+scp ./snapp_runner.cfg $MY_SSH_ACCOUNT:$MY_SC_DESTINATION ;
 
 echo "INFO      | $(date) |          Also copying sbatch_file to supercomputer..."
-scp ./SNAPPRunner_batch_qsub.sh $MY_SSH_ACCOUNT:$MY_SC_DESTINATION
+scp ./SNAPPRunner_batch_qsub.sh $MY_SSH_ACCOUNT:$MY_SC_DESTINATION ;
 
 
 
@@ -545,14 +541,15 @@ HERE
 echo "INFO      | $(date) |          Finished copying run folders to supercomputer and submitting SNAPP jobs to queue!!"
 
 echo "INFO      | $(date) |          Cleaning up: removing temporary files from local machine..."
-	for (( i=1; i<="$MY_NUM_INDEP_RUNS"; i++ )); do rm ./*_"$i".xml; done
-	rm ./qsub_top.txt
-	rm ./cd_and_qsub_commands.txt
-	rm ./qsub_bottom.txt
-	rm ./SNAPPRunner_batch_qsub.sh
+	for (( i=1; i<="$MY_NUM_INDEP_RUNS"; i++ )); do rm ./*_"$i".xml; done ;
+	rm ./qsub_top.txt ;
+	rm ./cd_and_qsub_commands.txt ;
+	rm ./qsub_bottom.txt ;
+	rm ./SNAPPRunner_batch_qsub.sh ;
 
-echo "INFO      | $(date) |          Bye."
-
+echo "INFO      | $(date) | Done."
+echo "INFO      | $(date) | Bye.
+"
 #
 #
 #
