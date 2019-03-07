aaffd5446e956b3547d2353257fe91f7cbf78dd4
Justin C. Bagley <justincbagley@users.noreply.github.com>
2019-03-06 22:30:48 -0600

    Update dadiRunner.sh

--- dadiRunner/dadiRunner.sh
+++ dadiRunner/dadiRunner.sh
@@ -6,12 +6,12 @@
 # |                                                                                      #
 #                                                                                        #
 # File: dadiRunner.sh                                                                    #
-  version="v0.1.1"                                                                       #
+  VERSION="v0.1.1"                                                                       #
 # Author: Justin C. Bagley                                                               #
-# Date: created by Justin Bagley on Thu, 20 Apr 2017 17:14:32 -0400                      #
+# Date: Created by Justin Bagley on Thu, 20 Apr 2017 17:14:32 -0400.                     #
 # Last update: May 16, 2017                                                              #
 # Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.                         #
-# Please report bugs to <bagleyj@umsl.edu>                                               #
+# Please report bugs to <bagleyj@umsl.edu>.                                              #
 #                                                                                        #
 # Description:                                                                           #
 # SHELL SCRIPT FOR AUTOMATING RUNNING ∂a∂i ON A REMOTE SUPERCOMPUTING CLUSTER            #
@@ -26,10 +26,10 @@ MY_SC_WALLTIME=48:00:00
 MY_DADI_VERSION_SWITCH=0
 
 ############ CREATE USAGE & HELP TEXTS
-Usage="Usage: $(basename "$0") [Help: -h help H Help] [Options: -i n w] workingDir 
+USAGE="Usage: $(basename $0) [Help: -h help H Help] [Options: -i n w v V --version] [stdin:] <workingDir> 
  ## Help:
-  -h   help text (also: -help)
-  -H   verbose help text (also: -Help)
+  -h   help text (also: --help) echo this help text and exit
+  -H   verbose help text (also: -Help) echo verbose help text and exit
 
  ## Options:
   -i   SNPInput (def: $MY_SNP_DATA_FILE) SNP data input file
@@ -63,14 +63,14 @@ Usage="Usage: $(basename "$0") [Help: -h help H Help] [Options: -i n w] workingD
  	demographic history of multiple populations from multidimensional SNP frequency data. 
  	PLOS Genetics 5(10): e1000695.
 
-Created by Justin Bagley on Thu, 20 Apr 2017 17:14:32 -0400
+Created by Justin Bagley on Thu, 20 Apr 2017 17:14:32 -0400.
 Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.
 "
 
-verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -i n w] workingDir 
+VERBOSE_USAGE="Usage: $(basename $0) [Help: -h help H Help] [Options: -i n w v V --version] [stdin:] <workingDir> 
  ## Help:
-  -h   help text (also: -help)
-  -H   verbose help text (also: -Help)
+  -h   help text (also: --help) echo this help text and exit
+  -H   verbose help text (also: -Help) echo verbose help text and exit
 
  ## Options:
   -i   SNPInput (def: $MY_SNP_DATA_FILE) SNP data input file
@@ -120,12 +120,12 @@ verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -i n w] wo
  prior to passing each .py file to Python. An example virtualenv.txt block is provided.
  
 		## Usage examples: 
-		"$0" .
-		"$0" -n 5 .
-		"$0" -i <SNPInput filename> .
-		"$0" -i <SNPInput filename> -n 20 .			## Ex.: changing no. independent runs.
-		"$0" -i <SNPInput filename> -v 1 .			## Ex.: changing to ∂a∂i-mod version (requires virtualenv.txt file).
-		"$0" -i <SNPInput filename> -n 20 -w 24:00:00 .	## Ex.: changing run walltime.
+		$0 .
+		$0 -n 5 .
+		$0 -i <SNPInput filename> .
+		$0 -i <SNPInput filename> -n 20 .			## Ex.: changing no. independent runs.
+		$0 -i <SNPInput filename> -v 1 .			## Ex.: changing to ∂a∂i-mod version (requires virtualenv.txt file).
+		$0 -i <SNPInput filename> -n 20 -w 24:00:00 .	## Ex.: changing run walltime.
 	
  CITATION
  Bagley, J.C. 2019. PIrANHA v0.1.7. GitHub repository, Available at: 
@@ -136,41 +136,39 @@ verboseHelp="Usage: $(basename "$0") [Help: -h help H Help] [Options: -i n w] wo
  	demographic history of multiple populations from multidimensional SNP frequency data. 
  	PLOS Genetics 5(10): e1000695.
 
-Created by Justin Bagley on Thu, 20 Apr 2017 17:14:32 -0400
+Created by Justin Bagley on Thu, 20 Apr 2017 17:14:32 -0400.
 Copyright (c) 2017-2019 Justin C. Bagley. All rights reserved.
 "
 
 if [[ "$1" == "-h" ]] || [[ "$1" == "-help" ]]; then
-	echo "$Usage"
+	echo "$USAGE"
 	exit
 fi
 
 if [[ "$1" == "-H" ]] || [[ "$1" == "-Help" ]]; then
-	echo "$verboseHelp"
+	echo "$VERBOSE_USAGE"
 	exit
 fi
 
-if [[ "$1" == "-v" ]] || [[ "$1" == "--version" ]]; then
-	echo "$(basename $0) ${version}";
+if [[ "$1" == "-V" ]] || [[ "$1" == "--version" ]]; then
+	echo "$(basename $0) $VERSION";
 	exit
 fi
 
 ############ PARSE THE OPTIONS
 while getopts 'i:n:w:v:' opt ; do
   case $opt in
-
 ## ∂a∂i options:
     i) MY_SNP_DATA_FILE=$OPTARG ;;
     n) MY_NUM_INDEP_RUNS=$OPTARG ;;
     w) MY_SC_WALLTIME=$OPTARG ;;
     v) MY_DADI_VERSION_SWITCH=$OPTARG ;;
-
 ## Missing and illegal options:
     :) printf "Missing argument for -%s\n" "$OPTARG" >&2
-       echo "$Usage" >&2
+       echo "$USAGE" >&2
        exit 1 ;;
    \?) printf "Illegal option: -%s\n" "$OPTARG" >&2
-       echo "$Usage" >&2
+       echo "$USAGE" >&2
        exit 1 ;;
   esac
 done
@@ -179,18 +177,16 @@ done
 shift $((OPTIND-1)) 
 # Check for mandatory positional parameters
 if [ $# -lt 1 ]; then
-echo "$Usage"
-  exit 1
+	echo "$USAGE"
+	exit 1
 fi
 USER_SPEC_PATH="$1"
-
-echo "INFO      | $(date) |          Setting user-specified path to: "
-echo "$USER_SPEC_PATH "	
 
 echo "
 ##########################################################################################
 #                             dadiRunner v0.1.1, April 2017                              #
-##########################################################################################"
+##########################################################################################
+"
 
 ######################################## START ###########################################
 echo "INFO      | $(date) | Starting dadiRunner pipeline... "
@@ -198,11 +194,16 @@ echo "INFO      | $(date) | STEP #1: SETUP. "
 echo "INFO      | $(date) |          Setting up variables, including those specified in the cfg file..."
 ###### Set new path/dir environmental variable to user specified path, then create useful
 ##--shell functions and variables:
-if [[ "$USER_SPEC_PATH" = "$(echo $(pwd))" ]]; then
-	MY_WD_PATH=`pwd -P`
+if [[ "$USER_SPEC_PATH" = "$(echo $(pwd))" ]] || [[ "$USER_SPEC_PATH" = "." ]]; then
+	MY_WD_PATH="$(pwd -P)";
+	echo "INFO      | $(date) |          Setting working directory to: $MY_WD_PATH "
+elif [[ "$USER_SPEC_PATH" = ".." ]] || [[ "$USER_SPEC_PATH" = "../" ]] || [[ "$USER_SPEC_PATH" = "..;" ]] || [[ "$USER_SPEC_PATH" = "../;" ]]; then
+	cd ..;
+	MY_WD_PATH="$(pwd -P)";
 	echo "INFO      | $(date) |          Setting working directory to: $MY_WD_PATH "
 elif [[ "$USER_SPEC_PATH" != "$(echo $(pwd))" ]]; then
-	MY_WD_PATH=$USER_SPEC_PATH
+	MY_WD_PATH=$USER_SPEC_PATH;
+	cd "$MY_WD_PATH";
 	echo "INFO      | $(date) |          Setting working directory to: $MY_WD_PATH "	
 else
 	echo "WARNING!  | $(date) |          Null working directory path. Quitting... "
@@ -211,20 +212,19 @@ fi
 	calc () { 
 		bc -l <<< "$@" 
 }
-	MY_INPUT_PY_FILES=./*.py
-	MY_NUM_PY_FILES="$(ls . | grep "\.py$" | wc -l)"
+	MY_INPUT_PY_FILES=./*.py;
+	MY_NUM_PY_FILES="$(ls . | grep "\.py$" | wc -l)";
 echo "INFO      | $(date) |          Number of .py ∂a∂i input files read: $MY_NUM_PY_FILES"	## Check number of input files read into program.
 	MY_SSH_ACCOUNT="$(grep -n "ssh_account" ./dadi_runner.cfg | \
-	awk -F"=" '{print $NF}')"
+	awk -F"=" '{print $NF}')";
 	MY_SC_DESTINATION="$(grep -n "destination_path" ./dadi_runner.cfg | \
-	awk -F"=" '{print $NF}' | sed 's/\ //g')"
+	awk -F"=" '{print $NF}' | sed 's/\ //g')";
 	MY_SC_BIN="$(grep -n "bin_path" ./dadi_runner.cfg | \
-	awk -F"=" '{print $NF}' | sed 's/\ //g')"
+	awk -F"=" '{print $NF}' | sed 's/\ //g')";
 	MY_EMAIL_ACCOUNT="$(grep -n "email_account" ./dadi_runner.cfg | \
-	awk -F"=" '{print $NF}')"
+	awk -F"=" '{print $NF}')";
 	MY_SC_PBS_WKDIR_CODE="$(grep -n "pbs_wkdir_code" ./dadi_runner.cfg | \
-	awk -F"=" '{print $NF}')"
-	## MY_NUM_INDEP_RUNS="$(grep -n "n_runs" ./dadi_runner.cfg | awk -F"=" '{print $NF}')"
+	awk -F"=" '{print $NF}')";
 
 
 echo "INFO      | $(date) | STEP #2: MAKE 9 COPIES PER INPUT .PY FILE FOR A TOTAL OF $MY_NUM_INDEP_RUNS RUNS OF EACH MODEL or "
@@ -249,7 +249,7 @@ end in 'run.py'."
 		done
 	)
 
-	rm ./*run.py		## Remove the original "run.py" input files so that only .py files
+	rm ./*run.py ;		## Remove the original "run.py" input files so that only .py files
 	            		## annotated with their run numbers 1 - $MY_NUM_INDEP_RUNS remain.
 
 
@@ -268,8 +268,8 @@ INPUT FILE FOR DIRECTING EACH RUN; MAKE SCRIPTS SPECIFIC TO ∂a∂i VERSION IND
 if [[ "$MY_DADI_VERSION_SWITCH" -eq "0" ]]; then
 	(
 		for i in $MY_INPUT_PY_FILES; do
-			mkdir "$(ls ${i} | sed 's/\.py$//g')"
-			MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')"
+			mkdir "$(ls ${i} | sed 's/\.py$//g')";
+			MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')";
 	
 echo "#!/bin/bash
 
@@ -290,10 +290,10 @@ $MY_SC_PBS_WKDIR_CODE
 
 exit 0" > dadi_pbs.sh
 
-			chmod +x dadi_pbs.sh
-			mv ./dadi_pbs.sh ./"$(ls ${i} | sed 's/.py$//g')"
-			cp $MY_SNP_DATA_FILE ./"$(ls ${i} | sed 's/\.py$//g')"
-			cp $i ./"$(ls ${i} | sed 's/\.py$//g')"
+			chmod +x dadi_pbs.sh ;
+			mv ./dadi_pbs.sh ./"$(ls ${i} | sed 's/.py$//g')" ;
+			cp $MY_SNP_DATA_FILE ./"$(ls ${i} | sed 's/\.py$//g')" ;
+			cp $i ./"$(ls ${i} | sed 's/\.py$//g')" ;
 		done
 	)
 
@@ -301,8 +301,8 @@ elif [[ "$MY_DADI_VERSION_SWITCH" -eq "1" ]]; then
 
 	(
 		for i in $MY_INPUT_PY_FILES; do
-			mkdir "$(ls ${i} | sed 's/\.py$//g')"
-			MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')"
+			mkdir "$(ls ${i} | sed 's/\.py$//g')";
+			MY_INPUT_BASENAME="$(ls ${i} | sed 's/^.\///g; s/.py$//g')";
 	
 echo "#!/bin/bash
 
@@ -324,19 +324,18 @@ $MY_SC_PBS_WKDIR_CODE
 
 exit 0" > dadi_pbs.sh
 
-			chmod +x dadi_pbs.sh
-			mv ./dadi_pbs.sh ./"$(ls ${i} | sed 's/.py$//g')"
-			cp $MY_SNP_DATA_FILE ./"$(ls ${i} | sed 's/\.py$//g')"
-			cp $i ./"$(ls ${i} | sed 's/\.py$//g')"
+			chmod +x dadi_pbs.sh ;
+			mv ./dadi_pbs.sh ./"$(ls ${i} | sed 's/.py$//g')" ;
+			cp $MY_SNP_DATA_FILE ./"$(ls ${i} | sed 's/\.py$//g')" ;
+			cp $i ./"$(ls ${i} | sed 's/\.py$//g')" ;
 		done
 	)
 fi
 
 
 echo "INFO      | $(date) |          Setup and run check on the number of run folders created by the program..."
-	MY_FILECOUNT="$(find . -type f | wc -l)"
-	MY_DIRCOUNT="$(find . -type d | wc -l)"
-	MY_NUM_RUN_FOLDERS="$(calc $MY_DIRCOUNT - 1)"
+	MY_DIRCOUNT="$(find . -type d | wc -l)";
+	MY_NUM_RUN_FOLDERS="$(calc $MY_DIRCOUNT - 1)";
 	echo "INFO      | $(date) |          Number of run folders created: $MY_NUM_RUN_FOLDERS"
 
 
@@ -361,8 +360,8 @@ $(cat ./virtualenv.txt)
 
 	(
 		for j in ./*/; do
-			FOLDERNAME="$(echo $j | sed 's/\.\///g')"
-			scp -r $j $MY_SSH_ACCOUNT:$MY_SC_DESTINATION	## Safe copy to remote machine.
+			FOLDERNAME="$(echo $j | sed 's/\.\///g')";
+			scp -r $j $MY_SSH_ACCOUNT:$MY_SC_DESTINATION ;	## Safe copy to remote machine.
 
 echo "cd $MY_SC_DESTINATION$FOLDERNAME
 qsub dadi_pbs.sh
@@ -370,14 +369,13 @@ qsub dadi_pbs.sh
 
 		done
 	)
-## NOT RUN:	sed -i '' 1,3d ./cd_and_qsub_commands.txt		## First line of file attempts to cd to "./*/", but this will break the submission script. Here, we use sed to remove the first three lines, which contain the problematic cd line, a qsub line, and a blank line afterwards.
 
 echo "
 $MY_SC_PBS_WKDIR_CODE
 exit 0
 " > batch_qsub_bottom.txt
 
-cat batch_qsub_top.txt cd_and_qsub_commands.txt batch_qsub_bottom.txt > dadirunner_batch_qsub.sh
+cat batch_qsub_top.txt cd_and_qsub_commands.txt batch_qsub_bottom.txt > dadirunner_batch_qsub.sh ;
 
 
 ##--More flow control. Check to make sure batch_qsub.sh file was successfully created.
@@ -389,10 +387,10 @@ else
 fi
 
 echo "INFO      | $(date) |          Also copying configuration file to supercomputer..."
-scp ./dadi_runner.cfg $MY_SSH_ACCOUNT:$MY_SC_DESTINATION
+scp ./dadi_runner.cfg $MY_SSH_ACCOUNT:$MY_SC_DESTINATION ;
 
 echo "INFO      | $(date) |          Also copying batch_qsub_file to supercomputer..."
-scp ./dadirunner_batch_qsub.sh $MY_SSH_ACCOUNT:$MY_SC_DESTINATION
+scp ./dadirunner_batch_qsub.sh $MY_SSH_ACCOUNT:$MY_SC_DESTINATION ;
 
 
 
@@ -417,11 +415,11 @@ echo "INFO      | $(date) |          Finished copying run folders to supercomput
 
 echo "INFO      | $(date) | STEP #6: CLEANUP: REMOVE UNNECESSARY FILES. "
 echo "INFO      | $(date) |          Cleaning up: removing temporary files from local machine..."
-	for (( i=1; i<="$MY_NUM_INDEP_RUNS"; i++ )); do rm ./*_"$i".py; done
-	rm ./batch_qsub_top.txt
-	rm ./cd_and_qsub_commands.txt
-	rm ./batch_qsub_bottom.txt
-	rm ./dadirunner_batch_qsub.sh
+	for (( i=1; i<="$MY_NUM_INDEP_RUNS"; i++ )); do rm ./*_"$i".py; done;
+	rm ./batch_qsub_top.txt ;
+	rm ./cd_and_qsub_commands.txt ;
+	rm ./batch_qsub_bottom.txt ;
+	rm ./dadirunner_batch_qsub.sh ;
 
 echo "INFO      | $(date) | Done organizing and copying SNP data and model files to supercomputer and submitting ∂a∂i "
 echo "INFO      | $(date) | jobs to the queue, using the dadiRunner pipeline. "
